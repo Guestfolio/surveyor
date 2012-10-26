1270fc9cd8ce332367d7c6f6515d10ea1b7eb122
markedmondson <mark.edmondson@gmail.com>
2012-10-26 12:20:41 -0700

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #match 'surveys', :to                                      => 'surveyor#new', :as    => 'available_surveys', :via => :get
   #match 'surveys/:survey_code', :to                         => 'surveyor#create', :as => 'take_survey', :via       => :post
   #match 'surveys/:survey_code', :to                    => 'surveyor#export', :as => 'export_survey', :via     => :get
-  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#show', :as   => 'view_my_survey', :via    => :get
-  match 'surveys/:survey_code/:response_set_code/take', :to => 'surveyor#edit', :as   => 'edit_my_survey', :via    => :get
-  match 'surveys/:survey_code/:response_set_code', :to      => 'surveyor#update', :as => 'update_my_survey', :via  => :put
+  match 'surveys/:survey_code/:response_set_code', :to      => 'surveys#show', :controller => "surveys", :as   => 'view_my_survey', :via    => :get
+  match 'surveys/:survey_code/:response_set_code/take', :to => 'surveys#edit', :controller => "surveys", :as   => 'edit_my_survey', :via    => :get
+  match 'surveys/:survey_code/:response_set_code', :to      => 'surveys#update', :controller => "surveys", :as => 'update_my_survey', :via  => :put
 end

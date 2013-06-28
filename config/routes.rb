ac17e208046613644cd719615bb4b9bca69aabdb
markedmondson <mark.edmondson@gmail.com>
2013-06-28 12:36:54 -0700

    Update routes.rb
    Remove default surveyor index path, rewritten in our routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Surveyor::Engine.routes.draw  do
   with_options controller: "surveys" do |o|
-  	o.match '/', :to                                     		 => 'surveys#index',  :as => 'available_surveys', via: :get
   	o.match '/:survey_code/:response_set_code', :to      => 'surveys#show',   :as => 'view_my_survey',    via: :get
   	o.match '/:survey_code/:response_set_code/take', :to => 'surveys#edit',   :as => 'edit_my_survey',    via: :get
   	o.match '/:survey_code/:response_set_code', :to      => 'surveys#update', :as => 'update_my_survey',  via: :put
